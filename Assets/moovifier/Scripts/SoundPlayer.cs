4d1b3fb468170e028147107bd6e8ac08ebdc069c
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-19 18:32:25 +1100

    stop randomness

--- Assets/moovifier/Scripts/SoundPlayer.cs
+++ Assets/moovifier/Scripts/SoundPlayer.cs
@@ -8,9 +8,6 @@ public class SoundPlayer : MonoBehaviour
 
     public void SoundEvent()
     {
-        // decide if a sound should be played 50% chance
-        if (Random.Range(0, 2) == 0) return;
-
         int index = Random.Range(0, zoomSounds.Length);
         AudioSource.PlayClipAtPoint(zoomSounds[index], transform.position);
     }
